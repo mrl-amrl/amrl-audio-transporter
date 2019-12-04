eee8df0f5d0b5185acb677ba57ec2ef4854f0d07
Ahmadreza Zibaei <ahmadrezazibaei@hotmail.com>
2019-12-04 19:04:23 +0330

    optimized

--- src/player.cpp
+++ src/player.cpp
@@ -5,10 +5,14 @@
 #include <mutex>
 #include <jack/jack.h>
 #include "ros/ros.h"
+#include "std_msgs/Bool.h"
 #include "audio_transporter/Audio.h"
 
 jack_port_t *output_port;
 jack_client_t *client;
+ros::Subscriber sub;
+ros::NodeHandle n;
+bool is_enabled = false;
 
 jack_default_audio_sample_t *ros2jack_buffer;
 unsigned int buffer_size;
@@ -54,21 +58,34 @@ void jackaudioCallback(const audio_transporter::Audio::ConstPtr &msg)
 	mtx.unlock();
 }
 
+void audio_enable_callback(const std_msgs::Bool::ConstPtr& msg) {
+	if (msg->data && !is_enabled) {
+		printf("[audio-sender] Creating ROSJack_Subscriber node...\n");
+		sub = n.subscribe("audio", 1000, jackaudioCallback);
+		printf("[audio-sender] done.\n");
+		return;
+	}
+	if (!msg->data && is_enabled) {
+		sub.shutdown();
+		return;
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	const char *client_name = "jack_sender";
 
-	printf("Connecting to Jack Server...\n");
+	printf("[audio-player] Connecting to Jack Server...\n");
 	jack_options_t options = JackNoStartServer;
 	jack_status_t status;
 
 	client = jack_client_open(client_name, options, &status);
 	if (client == NULL)
 	{
-		printf("jack_client_open() failed, status = 0x%2.0x\n", status);
+		printf("[audio-player] jack_client_open() failed, status = 0x%2.0x\n", status);
 		if (status & JackServerFailed)
 		{
-			printf("Unable to connect to JACK server.\n");
+			printf("[audio-player] Unable to connect to JACK server.\n");
 		}
 		exit(1);
 	}
@@ -76,12 +93,12 @@ int main(int argc, char *argv[])
 	if (status & JackNameNotUnique)
 	{
 		client_name = jack_get_client_name(client);
-		printf("Warning: other agent with our name is running, `%s' has been assigned to us.\n", client_name);
+		printf("[audio-player] Warning: other agent with our name is running, `%s' has been assigned to us.\n", client_name);
 	}
 	jack_set_process_callback(client, jack_callback, 0);
 	jack_on_shutdown(client, jack_shutdown, 0);
 
-	printf("Engine sample rate: %d\n", jack_get_sample_rate(client));
+	printf("[audio-player] Engine sample rate: %d\n", jack_get_sample_rate(client));
 
 	buffer_size = jack_get_buffer_size(client) * 10;
 	ros2jack_buffer = (jack_default_audio_sample_t *)malloc(sizeof(jack_default_audio_sample_t) * buffer_size);
@@ -90,38 +107,37 @@ int main(int argc, char *argv[])
 
 	if ((output_port == NULL))
 	{
-		printf("Could not create agent ports. Have we reached the maximum amount of JACK agent ports?\n");
+		printf("[audio-player] Could not create agent ports. Have we reached the maximum amount of JACK agent ports?\n");
 		exit(1);
 	}
 	if (jack_activate(client))
 	{
-		printf("Cannot activate client.");
+		printf("[audio-player] Cannot activate client.");
 		exit(1);
 	}
 
-	printf("Agent activated.\n");
-	printf("Connecting ports... ");
+	printf("[audio-player] Agent activated.\n");
+	printf("[audio-player] Connecting ports... ");
 	const char **serverports_names;
 	serverports_names = jack_get_ports(client, NULL, NULL, JackPortIsPhysical | JackPortIsInput);
 	if (serverports_names == NULL)
 	{
-		printf("No available physical output (server input) ports.\n");
+		printf("[audio-player] No available physical output (server input) ports.\n");
 		exit(1);
 	}
 	if (jack_connect(client, jack_port_name(output_port), serverports_names[0]))
 	{
-		printf("Cannot connect output port.\n");
+		printf("[audio-player] Cannot connect output port.\n");
 		exit(1);
 	}
 	free(serverports_names);
 
-	printf("done.\n");
+	printf("[audio-player] done.\n");
 
-	printf("Creating ROSJack_Write node...");
+	printf("[audio-player] Creating ROSJack_Subscriber node...\n");
 	ros::init(argc, argv, client_name);
-	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("audio", 1000, jackaudioCallback);
-	printf(" done.\n");
+	n.subscribe("audio_enable", 1, audio_enable_callback);		
+	printf("[audio-player] done.\n");
 
 	ros::spin();
 
--- src/sender.cpp
+++ src/sender.cpp
@@ -4,15 +4,20 @@
 #include <string.h>
 #include <jack/jack.h>
 #include "ros/ros.h"
+#include "std_msgs/Bool.h"
 #include "audio_transporter/Audio.h"
 
 jack_port_t *input_port;
 jack_client_t *client;
+ros::NodeHandle n;
+bool is_enabled = false;
 
 ros::Publisher rosjack_out;
 
 int jack_callback(jack_nframes_t nframes, void *arg)
 {
+	if (rosjack_out.getNumSubscribers() == 0) return 0;
+
 	jack_default_audio_sample_t *in = (jack_default_audio_sample_t *)jack_port_get_buffer(input_port, nframes);
 
 	audio_transporter::Audio msg;
@@ -20,10 +25,8 @@ int jack_callback(jack_nframes_t nframes, void *arg)
 	msg.channels = 1;
 
 	msg.data.resize(nframes);
-	for (int i = 0; i < nframes; i++)
-	{
-		msg.data[i] = in[i];
-	}
+	for (int i = 0; i < nframes; i++)	
+		msg.data[i] = in[i];			
 	rosjack_out.publish(msg);
 	return 0;
 }
@@ -33,27 +36,37 @@ void jack_shutdown(void *arg)
 	exit(1);
 }
 
+void audio_enable_callback(const std_msgs::Bool::ConstPtr& msg) {
+	if (msg->data && !is_enabled) {
+		printf("[audio-sender] Creating ROSJack_Publisher node...\n");
+		rosjack_out = n.advertise<audio_transporter::Audio>("audio", 1000);
+		printf("[audio-sender] done.\n");
+		return;
+	}
+	if (!msg->data && is_enabled) {
+		rosjack_out.shutdown();
+		return;
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	const char *client_name = "jack_player";
+	
+	ros::init(argc, argv, client_name);	
+	n.subscribe("audio_enable", 1, audio_enable_callback);		
 
-	printf("Creating ROSJack_Read node...");
-	ros::init(argc, argv, client_name);
-	ros::NodeHandle n;
-	rosjack_out = n.advertise<audio_transporter::Audio>("audio", 1000);
-	printf(" done.\n");
-
-	printf("Connecting to Jack Server...\n");
+	printf("[audio-sender] Connecting to Jack Server...\n");
 	jack_options_t options = JackNoStartServer;
 	jack_status_t status;
 
 	client = jack_client_open(client_name, options, &status);
 	if (client == NULL)
 	{
-		printf("jack_client_open() failed, status = 0x%2.0x\n", status);
+		printf("[audio-sender] jack_client_open() failed, status = 0x%2.0x\n", status);
 		if (status & JackServerFailed)
 		{
-			printf("Unable to connect to JACK server.\n");
+			printf("[audio-sender] Unable to connect to JACK server.\n");
 		}
 		exit(1);
 	}
@@ -61,40 +74,40 @@ int main(int argc, char *argv[])
 	if (status & JackNameNotUnique)
 	{
 		client_name = jack_get_client_name(client);
-		printf("Warning: other agent with our name is running, `%s' has been assigned to us.\n", client_name);
+		printf("[audio-sender] Warning: other agent with our name is running, `%s' has been assigned to us.\n", client_name);
 	}
 	jack_set_process_callback(client, jack_callback, 0);
 	jack_on_shutdown(client, jack_shutdown, 0);
-	printf("Engine sample rate: %d\n", jack_get_sample_rate(client));
+	printf("[audio-sender] Engine sample rate: %d\n", jack_get_sample_rate(client));
 	input_port = jack_port_register(client, "input", JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
 	if ((input_port == NULL))
 	{
-		printf("Could not create agent ports. Have we reached the maximum amount of JACK agent ports?\n");
+		printf("[audio-sender] Could not create agent ports. Have we reached the maximum amount of JACK agent ports?\n");
 		exit(1);
 	}
 	if (jack_activate(client))
 	{
-		printf("Cannot activate client.");
+		printf("[audio-sender] Cannot activate client.");
 		exit(1);
 	}
 
-	printf("Agent activated.\n");
-	printf("Connecting ports... ");
+	printf("[audio-sender] Agent activated.\n");
+	printf("[audio-sender] Connecting ports... ");
 
 	const char **serverports_names;
 	serverports_names = jack_get_ports(client, NULL, NULL, JackPortIsPhysical | JackPortIsOutput);
 	if (serverports_names == NULL)
 	{
-		printf("No available physical capture (server output) ports.\n");
+		printf("[audio-sender] No available physical capture (server output) ports.\n");
 		exit(1);
 	}
 	if (jack_connect(client, serverports_names[0], jack_port_name(input_port)))
 	{
-		printf("Cannot connect input port.\n");
+		printf("[audio-sender] Cannot connect input port.\n");
 		exit(1);
 	}
 	free(serverports_names);
-	printf("done.\n");
+	printf("[audio-sender] done.\n");
 	ros::spin();
 	jack_client_close(client);
 	exit(0);
